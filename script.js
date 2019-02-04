bf98e2c4fb17b21c953685b59b78d2ba242c7237
Alex <alex.y.kuznetsov@gmail.com>
2019-02-04 22:44:09 +0500

    one-sided 2

--- script.js
+++ script.js
@@ -2,20 +2,24 @@
 
 var heading = document.getElementById('heading');
 var para = document.getElementById('paragraph');
+// heading.style.fontSize = '300px';
 
 var calcFontSize = function () {
-	var currentParaWidth = para.offsetWidth + 'px';
-	heading.style.fontSize = '300px';
-	heading.style.width = currentParaWidth;
-	var headingSize = +heading.style.fontSize.slice(0, -2);
-	var paraSize = +currentParaWidth.slice(0, -2);
 
-	while (headingSize > paraSize) {
+	var currentParaWidth = para.offsetWidth + 'px';
+	var currentHeadingWidth = heading.offsetWidth + 'px';
+	var headingWidth = +currentHeadingWidth.slice(0, -2);
+	var headingSize = +heading.style.fontSize.slice(0, -2);
+	var paraWidth = +currentParaWidth.slice(0, -2);
+
+	while (headingWidth > paraWidth) {
 		headingSize --;
+		headingWidth --;
 	}
 
-	while (headingSize < paraSize) {
+	while (headingWidth < paraWidth) {
 		headingSize ++;
+		headingWidth ++;
 	}
 
 	heading.style.fontSize = headingSize + 'px';
@@ -24,4 +28,27 @@ var calcFontSize = function () {
 calcFontSize();
 
 
-// Сложение мух с котлетами. Как перевести длину параграфа в размер шрифта заголовка?
+
+
+// var heading = document.getElementById('heading');
+// var para = document.getElementById('paragraph');
+
+// var calcFontSize = function () {
+// 	var currentParaWidth = para.offsetWidth + 'px';
+// 	heading.style.fontSize = '300px';
+// 	heading.style.width = currentParaWidth;
+// 	var headingSize = +heading.style.fontSize.slice(0, -2);
+// 	var paraSize = +currentParaWidth.slice(0, -2);
+
+// 	while (headingSize > paraSize) {
+// 		headingSize --;
+// 	}
+
+// 	while (headingSize < paraSize) {
+// 		headingSize ++;
+// 	}
+
+// 	heading.style.fontSize = headingSize + 'px';
+// }
+
+// calcFontSize();
