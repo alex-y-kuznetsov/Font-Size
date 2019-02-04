e677f1a15d6cb526c8512fc6b0dac6bdf76675fe
Alex <alex.y.kuznetsov@gmail.com>
2019-02-04 22:45:44 +0500

    remove comment

--- script.js
+++ script.js
@@ -28,27 +28,3 @@ var calcFontSize = function () {
 calcFontSize();
 
 
-
-
-// var heading = document.getElementById('heading');
-// var para = document.getElementById('paragraph');
-
-// var calcFontSize = function () {
-// 	var currentParaWidth = para.offsetWidth + 'px';
-// 	heading.style.fontSize = '300px';
-// 	heading.style.width = currentParaWidth;
-// 	var headingSize = +heading.style.fontSize.slice(0, -2);
-// 	var paraSize = +currentParaWidth.slice(0, -2);
-
-// 	while (headingSize > paraSize) {
-// 		headingSize --;
-// 	}
-
-// 	while (headingSize < paraSize) {
-// 		headingSize ++;
-// 	}
-
-// 	heading.style.fontSize = headingSize + 'px';
-// }
-
-// calcFontSize();
