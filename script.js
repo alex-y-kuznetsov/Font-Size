5fe89c42241c65064b1d16f6e80caa110ff06ef6
Alex <alex.y.kuznetsov@gmail.com>
2019-02-04 23:50:11 +0500

    math is hard

--- script.js
+++ script.js
@@ -2,24 +2,38 @@
 
 var heading = document.getElementById('heading');
 var para = document.getElementById('paragraph');
-// heading.style.fontSize = '300px';
+heading.style.fontSize = '300px';
 
 var calcFontSize = function () {
 
-	var currentParaWidth = para.offsetWidth + 'px';
-	var currentHeadingWidth = heading.offsetWidth + 'px';
-	var headingWidth = +currentHeadingWidth.slice(0, -2);
-	var headingSize = +heading.style.fontSize.slice(0, -2);
-	var paraWidth = +currentParaWidth.slice(0, -2);
+	var headingWidth = Math.round(getComputedStyle(heading).width.slice(0, -2));
+	var headingSize = +getComputedStyle(heading).fontSize.slice(0, -2);
+	var paraWidth = Math.round(getComputedStyle(para).width.slice(0, -2));
 
-	while (headingWidth > paraWidth) {
-		headingSize --;
-		headingWidth --;
+	var headingToSmall = function () {
+		while (headingWidth > paraWidth) {
+			headingWidth--;
+			headingSize = headingWidth / 4;
+		}
+		console.log(headingSize);
+		heading.style.width = headingWidth + 'px';
+		return headingSize;
 	}
 
-	while (headingWidth < paraWidth) {
-		headingSize ++;
-		headingWidth ++;
+	var headingToLarge = function () {
+		while (headingWidth < paraWidth) {
+			headingWidth++;
+			headingSize++;
+		}
+		return headingSize;
+	}
+
+	if (headingWidth > paraWidth) {
+		headingToSmall();
+		console.log('heading larger');
+	} else {
+		headingToLarge();
+		console.log('heading smaller')
 	}
 
 	heading.style.fontSize = headingSize + 'px';
@@ -28,3 +42,4 @@ var calcFontSize = function () {
 calcFontSize();
 
 
+// Математика сложно
