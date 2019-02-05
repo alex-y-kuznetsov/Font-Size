78b9dcf96315a8c768e98dd3fcca5da88b74b88b
Damir <zs.keda@gmail.com>
2019-02-05 11:00:13 +0500

    new version

--- script.js
+++ script.js
@@ -2,41 +2,28 @@
 
 var heading = document.getElementById('heading');
 var para = document.getElementById('paragraph');
-heading.style.fontSize = '300px';
 
 var calcFontSize = function () {
 
-	var headingWidth = Math.round(getComputedStyle(heading).width.slice(0, -2));
-	var headingSize = +getComputedStyle(heading).fontSize.slice(0, -2);
-	var paraWidth = Math.round(getComputedStyle(para).width.slice(0, -2));
+	var headingWidth = Math.round(getComputedStyle(heading).width.slice(0, -2)); // текущая ширина
+	var headingSize = +getComputedStyle(heading).fontSize.slice(0, -2); // размер h1
+	var paraWidth = Math.round(getComputedStyle(para).width.slice(0, -2)); // ширина колонки
 
-	var headingToSmall = function () {
-		while (headingWidth > paraWidth) {
-			headingWidth--;
-			headingSize = headingWidth / 4;
+	if(headingWidth > paraWidth){
+		for(var i=headingSize*10; headingWidth > paraWidth; --i){
+			document.getElementById('heading').style.fontSize = i/10 + "px";
+			paraWidth = Math.round(getComputedStyle(para).width.slice(0, -2)); // ширина колонки
+			headingWidth = Math.round(getComputedStyle(heading).width.slice(0, -2)); // текущая ширина
 		}
-		console.log(headingSize);
-		heading.style.width = headingWidth + 'px';
-		return headingSize;
-	}
-
-	var headingToLarge = function () {
-		while (headingWidth < paraWidth) {
-			headingWidth++;
-			headingSize++;
+	}else{
+		for(var i=headingSize*10; headingWidth < paraWidth; ++i){
+			document.getElementById('heading').style.fontSize = i/10 + "px";
+			paraWidth = Math.round(getComputedStyle(para).width.slice(0, -2)); // ширина колонки
+			headingWidth = Math.round(getComputedStyle(heading).width.slice(0, -2)); // текущая ширина
 		}
-		return headingSize;
 	}
 
-	if (headingWidth > paraWidth) {
-		headingToSmall();
-		console.log('heading larger');
-	} else {
-		headingToLarge();
-		console.log('heading smaller')
-	}
-
-	heading.style.fontSize = headingSize + 'px';
+	
 }
 
 calcFontSize();
